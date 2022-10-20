88e08b3e9544d6a21ddfd054e233c63ae62a6022
BSD-Yassin <Yassin.bousaadi@gmail.com>
2022-10-20 14:47:04 +0200

    "Added readme specifications"

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Crushbase Logger
 
 Small Crushbase Logger coded on Python with undetected browser and Selenium. This is very experimental, it will only work with intervals of seconds.
+
+## How to install 
+
+```
+git clone https://github.com/BSD-Yassin/Undetected_logger 
+cd Undetected_logger
+
+# Optionally use a venv for less conflict 
+# python -m venv venv && source venv/bin/activate
+
+pip -r install requirements.txt
+```
+
+## Configuration
+
+Do change the line 24 and 25 with your own **login** and **password**
+
+## Notes 
+
+This will not work if you overuse it within several minutes as the bot detection will flag this as bot use, a better solution is to log once and reuse cookies for several selenium sessions. An even better way would be to use the internal API of the website if it can be reverse engineered for everything.
--- main.py
+++ main.py
@@ -9,13 +9,21 @@ agent = "Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0"
 options.add_argument('--enable-javascript')
 options.add_argument("--disable-extensions")
 options.add_argument('--profile-directory=Default')
-options.add_argument("--incognito")
+# options.add_argument("--incognito")
 options.add_argument("--disable-plugins-discovery")
 options.add_argument("--start-maximized")
 
 driver = uc.Chrome(options=options)
 
 driver.get('https://www.crunchbase.com/login/')
+
+login_PATH = "/html/body/chrome/div/mat-sidenav-container/mat-sidenav-content/div/authentication-page/page-layout/div/div/div[2]/authentication/mat-card/mat-tab-group/div/mat-tab-body[1]/div/login/form/mat-form-field[1]/div/div[1]/div/input"
+password_PATH = "/html/body/chrome/div/mat-sidenav-container/mat-sidenav-content/div/authentication-page/page-layout/div/div/div[2]/authentication/mat-card/mat-tab-group/div/mat-tab-body[1]/div/login/form/mat-form-field[2]/div/div[1]/div/input"
+button_login = "/html/body/chrome/div/mat-sidenav-container/mat-sidenav-content/div/authentication-page/page-layout/div/div/div[2]/authentication/mat-card/mat-tab-group/div/mat-tab-body[1]/div/login/form/button"
+
+driver.find_element(By.XPATH, login_PATH).send_keys("#PutyourUsernamehere")
+driver.find_element(By.XPATH, password_PATH).send_keys("#PutyourPasswordhere")
+driver.find_element(By.XPATH,button_login).click()
 
 # This only serves to keep the window open, otherwise Selenium will block it
 time.sleep(60)
